d66e2d3ae564b371e15ebab866a548abb97b0d36
Nick Hill <nicholaspatrickhill@gmail.com>
2022-09-25 15:05:59 -0400

    added the MakeList method

---
+++ MyInstruments2/MakeList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInstruments2
+{
+    public class MakeList
+    {
+        public static void InstrumentInventory()
+        {
+            string[] instrumentInventory =
+            {
+                    "-Fender Jazzmaster",
+                    "-Fender Jaguar",
+                    "-USA Fender Stratocaster",
+                    "-Japan Fender Stratocaster",
+                    "-Hofner Club",
+                    "-Fender Precision Bass"
+                    };
+            Array.Sort(instrumentInventory);
+            foreach (string i in instrumentInventory)
+                Console.WriteLine(i);
+        }
+    }
+}
+    
+
