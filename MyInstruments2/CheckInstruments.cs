813d5ccc22031afb8fac7787fcfac4744c1adcdb
Nick Hill <nicholaspatrickhill@gmail.com>
2022-09-25 15:24:44 -0400

    added USA strat to dictionary

--- MyInstruments2/CheckInstruments.cs
+++ MyInstruments2/CheckInstruments.cs
@@ -8,7 +8,7 @@
             {
                 { "Jazzmaster", new MusicalInstrument() { Make = "Fender", Model = "Jazzmaster", Type = "guitar", Country = "Japan", Year = 1997, Serial = "A019459", Color = "Sunburst", } },
                 { "Jaguar", new MusicalInstrument() { Make = "Fender", Model = "Jaguar", Type = "guitar", Country = "Japan", Year = 1997, Serial = "A035931", Color = "White" } },
-                /// MusicalInstrument StratUSA = new MusicalInstrument("Fender", "Stratocaster", "guitar", "USA", 2018, DateTime.Now.Year - 2018, "US18004688", "Black");
+                { "USA Stratocaster", new MusicalInstrument() { Make = "Fender", Model = "Stratocaster", Type ="guitar", Country = "USA", Year = 2018, Serial = "US18004688", Color = "Black", } },
                 /// MusicalInstrument StratJapan = new MusicalInstrument("Fender", "Stratocaster", "guitar", "Japan", 1993, DateTime.Now.Year - 1993, "P038686", "Transparent Blue");
                 /// MusicalInstrument Hofner = new MusicalInstrument("Hofner", "Club", "bass", "China", 0, 0, "X0501H152", "Black");
                 /// MusicalInstrument PrecisionBass = new MusicalInstrument("Fender", "Precision Bass", "bass", "Mexico", 2021, DateTime.Now.Year - 2021, "MX21254822", "Buttercream");
