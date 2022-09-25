e74d6d34577c71078b864a3a2bdc67dd90208f0b
Nick Hill <nicholaspatrickhill@gmail.com>
2022-09-25 15:54:58 -0400

    code comments

--- MyInstruments2/MakeList.cs
+++ MyInstruments2/MakeList.cs
@@ -8,6 +8,7 @@ namespace MyInstruments2
 {
     public class MakeList
     {
+        /// uses an array to generate a list of all instruments available to return in the Main program
         public static void InstrumentInventory()
         {
             string[] instrumentInventory =
