31e12ab3a500c90834dc0b80b387275282242730
Nick Hill <nicholaspatrickhill@gmail.com>
2022-11-02 18:11:39 -0400

    moved switch to its own class UserCommands

---
+++ MyInstruments2/UserCommands.cs
@@ -0,0 +1,56 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyInstruments
+{
+    public class UserCommands
+    {
+        private static readonly log4net.ILog log = LogHelper.GetLogger();
+        public static bool SelectCommand(bool repeat, Guitar guitar, Bass bass, Ukulele ukulele, string input)
+        {
+            switch (input)
+            {
+                case "Quit":
+                    repeat = false;
+                    break;
+                case "Commands":
+                    ConsoleWindow.PrintCommands();
+                    break;
+                case "All":
+                    Collection.ListAll();
+                    break;
+                case "Clear":
+                    ConsoleWindow.ClearConsole();
+                    break;
+                case "Guitars":
+                case "Basses":
+                case "Ukuleles":
+                    Collection.ListInstrument(guitar, bass, ukulele, input);
+                    break;
+                case "Count All":
+                    Collection.CountAll();
+                    break;
+                case "Count Guitars":
+                case "Count Basses":
+                case "Count Ukuleles":
+                    Collection.CountInstrument(guitar, bass, ukulele, input);
+                    break;
+                case "Print":
+                case "Print to File":
+                    Collection.PrintAllToFile();
+                    break;
+                default:
+                    // Fulfills feature list requirement: "Implement a log that records invalid inputs and writes them to a text file"
+                    log4net.GlobalContext.Properties["UserInput"] = input;
+                    log.Error("Invalid User Input");
+                    ConsoleWindow.PrintErrorMessage();
+                    break;
+            }
+            return repeat;
+        }
+    }
+}
--- MyInstruments2/UserSelection.cs
+++ MyInstruments2/UserSelection.cs
@@ -7,12 +7,12 @@ using static MyInstruments.Guitar;
 using static MyInstruments.Bass;
 using static MyInstruments.Ukulele;
 using static MyInstruments.MusicalInstrument;
+using static MyInstruments.UserCommands;
 
 namespace MyInstruments
 {
     public class UserSelection
     {
-        private static readonly log4net.ILog log = LogHelper.GetLogger();
         public static bool SelectInstrument(bool repeat, Guitar guitar, Bass bass, Ukulele ukulele, string input)
         {
             Console.ForegroundColor = ConsoleColor.White;
@@ -33,49 +33,6 @@ namespace MyInstruments
                 ukuleleChoice.PrintInstrument();
             }
             else repeat = SelectCommand(repeat, guitar, bass, ukulele, input);
-            return repeat;
-        }
-
-        static bool SelectCommand(bool repeat, Guitar guitar, Bass bass, Ukulele ukulele, string input)
-        {
-            switch (input)
-            {
-            case "Quit":
-                repeat = false;
-                break;
-            case "Commands":
-                ConsoleWindow.PrintCommands();
-                break;
-            case "All":
-                Collection.ListAll();
-                break;
-            case "Clear":
-                ConsoleWindow.ClearConsole();
-                break;
-            case "Guitars":
-            case "Basses":
-            case "Ukuleles":
-                Collection.ListInstrument(guitar, bass, ukulele, input);
-                break;
-            case "Count All":
-                Collection.CountAll();
-                break;
-            case "Count Guitars":
-            case "Count Basses":
-            case "Count Ukuleles":
-                Collection.CountInstrument(guitar, bass, ukulele, input);
-                break;
-            case "Print":
-            case "Print to File":
-                Collection.PrintAllToFile();
-                break;
-            default:
-                // Fulfills feature list requirement: "Implement a log that records invalid inputs and writes them to a text file"
-                log4net.GlobalContext.Properties["UserInput"] = input;
-                log.Error("Invalid User Input");
-                ConsoleWindow.PrintErrorMessage();
-                break;
-            }
             return repeat;
         }
     }  
