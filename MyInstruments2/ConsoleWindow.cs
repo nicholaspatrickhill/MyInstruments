5d7d1992564c1f05b15d847cb949c8011b334d98
Nick Hill <nicholaspatrickhill@gmail.com>
2022-11-07 07:08:34 -0500

    updated instructions

--- MyInstruments2/ConsoleWindow.cs
+++ MyInstruments2/ConsoleWindow.cs
@@ -41,20 +41,18 @@ namespace MyInstruments
             WriteLine();
             WriteLine("Use the UP and DOWN arrow keys to highlight the function that you wish to perform and then press enter.");
             WriteLine();
-            WriteLine("In \"Review Instruments\" you may review any instrument in my collection by typing its key and then pressing enter.");
-            WriteLine("You may review the keys available by typing \"Guitars\", \"Basses\", \"Keyboards\" or \"Other\" and then pressing enter.");
-            WriteLine("Please note that keys are case specific and you will need to follow the case as indicated by the list for valid input.");
-            WriteLine();
-            WriteLine("If the console is becoming clogged with text you may refresh it by typing \"Clear.\" and then pressing enter");
-            WriteLine();
-            WriteLine("Type \"Menu\" and press enter to return to the Main Menu and select another option.");
+            WriteLine("In \"Review Instruments\" you may review any instrument in my collection by typing its key and pressing enter.");
+            WriteLine(" -You may review the keys available by typing \"Guitars\", \"Basses\", \"Keyboards\" or \"Other\" and then pressing enter.");
+            WriteLine(" -Please note that keys are case specific and you will need to follow the case as indicated by the list for valid input.");
+            WriteLine(" -If the console is becoming clogged with text you may refresh it by typing \"Clear.\" and pressing enter");
+            WriteLine(" -Type \"Menu\" and press enter to return to the Main Menu and select another option.");
             WriteLine();
             WriteLine("\"List Instruments\" shows a complete list of the keys available to the program.");
             WriteLine();
             WriteLine("\"Count Instruments\" provides a count of each instrument type and the total number of instruments in the collection.");
             WriteLine();
             WriteLine("\"Save Text File\" saves a list of the instrument keys to a .txt file in the temp folder on the C:\\ drive.");
-            WriteLine("Tip: This can be a handy reference guide when woking in the \"Review Instruments\" module.");
+            WriteLine(" -Tip: This can be a handy reference guide when woking in the \"Review Instruments\" module.");
             WriteLine();
             WriteLine("\"Exit\" will terminate the program upon confirmation.");
         }
