8ababa0ac2bfeda652df051df14df070c3c8769b
Nick Hill <nicholaspatrickhill@gmail.com>
2022-11-10 07:09:50 -0500

    updated instructions

--- MyInstruments2/ConsoleMessage.cs
+++ MyInstruments2/ConsoleMessage.cs
@@ -70,7 +70,6 @@ namespace MyInstruments
             WriteLine("Use the UP and DOWN arrow keys to highlight the function that you wish to perform and then press enter.");
             WriteLine();
             WriteLine("In \"Review Instruments\" you may review any instrument in my collection by typing its key and pressing enter.");
-            WriteLine(" --Please note that all user keys and commands are lower case specific for valid input.");
             WriteLine(" --You may review the keys available by typing \"guitars\", \"basses\", \"keyboards\" or \"other\" and then pressing enter.");
             WriteLine(" --If the console is becoming clogged with text you may refresh it by typing \"clear\" and pressing enter");
             WriteLine(" --Type \"menu\" and press enter to return to the Main Menu and select another option.");
