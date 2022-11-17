e067c4dad26d4907df21f786d93ec0d21e4c1e49
Nick Hill <nicholaspatrickhill@gmail.com>
2022-11-17 14:02:38 -0500

    changed the Exit method to be a menu using a switch instead of requiring user input

--- MyInstruments2/MainMenu.cs
+++ MyInstruments2/MainMenu.cs
@@ -100,32 +100,54 @@ namespace MyInstruments
             RunMainMenu();
         }
 
+        //private static void ExitTheProgram()
+        //{
+        //    ForegroundColor = ConsoleColor.White;
+        //    WriteLine();
+        //    WriteLine("Are you sure you wish to exit the program?");
+        //    WriteLine("Type \"y\" and press enter to exit or type \"n\" and press enter to return to the Main Menu.");
+
+        //    ForegroundColor = ConsoleColor.Cyan;
+        //    string input = ReadLine() ?? String.Empty;
+
+        //    switch (input.ToLower())
+        //    {
+        //        case "y":
+        //            ForegroundColor = ConsoleColor.White;
+        //            Environment.Exit(0);
+        //            break;
+        //        case "n":
+        //            Start();
+        //            break;
+        //        default:
+        //            log4net.GlobalContext.Properties["UserInput"] = input;
+        //            log.Error("Invalid User Input");
+        //            ForegroundColor = ConsoleColor.White;
+        //            WriteLine();
+        //            WriteLine("Invalid Command. Please try again.");
+        //            ExitProgram();
+        //            break;
+        //    }
+
         private static void ExitTheProgram()
         {
+            Thread.Sleep(500);
             ForegroundColor = ConsoleColor.White;
-            WriteLine();
-            WriteLine("Are you sure you wish to exit the program?");
-            WriteLine("Type \"y\" and press enter to exit or type \"n\" and press enter to return to the Main Menu.");
+            string prompt = "\nAre you sure you wish to exit the program?";
+            string[] options = { "\nyes", "no" };
 
-            ForegroundColor = ConsoleColor.Cyan;
-            string input = ReadLine() ?? String.Empty;
+            Menu exitMenu = new Menu(prompt, options);
+            int selectedIndex = exitMenu.Run();
 
-            switch (input.ToLower())
+            switch (selectedIndex)
             {
-                case "y":
+                case 0:
                     ForegroundColor = ConsoleColor.White;
+                    WriteLine("\nRock on...");
                     Environment.Exit(0);
                     break;
-                case "n":
+                case 1:
                     Start();
-                    break;
-                default:
-                    log4net.GlobalContext.Properties["UserInput"] = input;
-                    log.Error("Invalid User Input");
-                    ForegroundColor = ConsoleColor.White;
-                    WriteLine();
-                    WriteLine("Invalid Command. Please try again.");
-                    ExitProgram();
                     break;
             }
         }   
